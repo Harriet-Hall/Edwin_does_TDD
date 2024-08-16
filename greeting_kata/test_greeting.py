c6756098fdb35fb1524c33737db4c6566ffd558c
Harriet <harriet.hall@madetech.com>
2024-08-16 08:46:47 +0100

    project setup

---
+++ greeting_kata/test_greeting.py
@@ -0,0 +1,6 @@
+# create a function that takes a name as an argument and returns a greeting message Hello + name!
+# eg if the function takes the name Edwin, it should return the greeting "Hello Edwin!"
+# if the function is not given a name it should return the greeting "Hello stranger!"
+# if the function is given an argument that is not a string it returns the message "This is not a valid name!"
+
+
